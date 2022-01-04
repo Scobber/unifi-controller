849bd281d48db3c3982437a9965866502724b975
Scobber <scott@pep.id.au>
2022-01-05 08:32:26 +1100

    add stop script

--- obj/entrypoint.sh
+++ obj/entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+cd /opt/UniFi
 bash /app/start.sh &
 while [ true ]; do
     sleep 2
-    tail -f /opt/UniFi/logs/server.log
+    tail -f /opt/UniFi/logs/server.log || break
 done
+bash /app/stop.sh
+tail -f /opt/UniFi/logs/server.log
---
+++ obj/stop.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd /opt/UniFi
+java -jar lib/ace.jar stop
