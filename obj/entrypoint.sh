430a1091e978952687d7403dea47773f1add0528
Scobber <scott@pep.id.au>
2022-01-05 10:00:29 +1100

    add break 2

--- obj/entrypoint.sh
+++ obj/entrypoint.sh
@@ -3,7 +3,7 @@ cd /opt/UniFi
 bash /app/start.sh &
 while [ true ]; do
     sleep 2
-    tail -f /opt/UniFi/logs/server.log || break
+    tail -f /opt/UniFi/logs/server.log || break 2
 done
 bash /app/stop.sh
 tail -f /opt/UniFi/logs/server.log
