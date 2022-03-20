da96b34e68012a98346682e1667b0a457461c3f6
Scobber <scott@pep.id.au>
2022-03-20 18:39:57 +1100

    Update build.sh
    Update to 7.0.25

--- obj/build.sh
+++ obj/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # this file is for any additional pipeline commands
 ls /app
-curl -o /tmp/unifi.zip https://dl.ui.com/unifi/7.0.23/UniFi.unix.zip
+curl -o /tmp/unifi.zip https://dl.ui.com/unifi/7.0.25-43e7fc6711/UniFi.unix.zip
 unzip  /tmp/unifi.zip -d /opt
 
 sleep 5
