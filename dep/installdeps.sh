bdfe0c24a5017ac9f94adcbec870db12471028fa
Scobber <scott@pep.id.au>
2022-06-04 12:33:21 +1000

    Update installdeps.sh
    Fix Dependency Libssl1.0

--- dep/installdeps.sh
+++ dep/installdeps.sh
@@ -5,13 +5,13 @@ wget -qO - https://www.mongodb.org/static/pgp/server-3.4.asc |  apt-key add -
 echo "deb http://repo.mongodb.org/apt/debian jessie/mongodb-org/3.4 main" | tee /etc/apt/sources.list.d/mongodb-org-3.4.list
 #curl -sSL https://dl.ui.com/unifi/unifi-repo.gpg -o /etc/apt/trusted.gpg.d/unifi-repo.gpg
 wget http://ftp.us.debian.org/debian/pool/main/g/glibc/multiarch-support_2.28-10_amd64.deb
-wget http://security.debian.org/debian-security/pool/updates/main/o/openssl/libssl1.0.0_1.0.1t-1+deb8u12_amd64.deb
+wget http://security.ubuntu.com/ubuntu/pool/main/o/openssl1.0/libssl1.0.0_1.0.2n-1ubuntu5.9_amd64.deb
 #wget -qO - https://adoptopenjdk.jfrog.io/adoptopenjdk/api/gpg/key/public | apt-key add -
 #add-apt-repository --yes https://adoptopenjdk.jfrog.io/adoptopenjdk/deb/
 
 
 dpkg -i multiarch-support_2.28-10_amd64.deb
-dpkg -i libssl1.0.0_1.0.1t-1+deb8u12_amd64.deb
+dpkg -i libssl1.0.0_1.0.2n-1ubuntu5.9_amd64.deb
 rm *.deb
 
 apt update
