de4ab14a83161ebda78306d3afe24be52a4d58a2
Scobber <scott@pep.id.au>
2022-06-11 16:37:33 +1000

    Update installdeps.sh

--- dep/installdeps.sh
+++ dep/installdeps.sh
@@ -1,8 +1,8 @@
 apt update
 apt install -y software-properties-common curl wget gnupg gpg
 #echo 'deb https://www.ui.com/downloads/unifi/debian stable ubiquiti' | tee /etc/apt/sources.list.d/100-ubnt-unifi.list
-wget -qO - https://www.mongodb.org/static/pgp/server-3.4.asc |  apt-key add -
-echo "deb http://repo.mongodb.org/apt/debian jessie/mongodb-org/3.4 main" | tee /etc/apt/sources.list.d/mongodb-org-3.4.list
+wget -qO - https://www.mongodb.org/static/pgp/server-3.6.asc |  apt-key add -
+echo "deb http://repo.mongodb.org/apt/debian jessie/mongodb-org/3.6 main" | tee /etc/apt/sources.list.d/mongodb-org-3.6.list
 #curl -sSL https://dl.ui.com/unifi/unifi-repo.gpg -o /etc/apt/trusted.gpg.d/unifi-repo.gpg
 wget http://ftp.us.debian.org/debian/pool/main/g/glibc/multiarch-support_2.28-10_amd64.deb
 wget http://security.ubuntu.com/ubuntu/pool/main/o/openssl1.0/libssl1.0.0_1.0.2n-1ubuntu5.9_amd64.deb
