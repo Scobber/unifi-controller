9ed40eef1be8ac03a7b3ce6979042afbdba63a56
scobber <scott@pep.id.au>
2022-06-11 16:12:01 +1000

    Update build.sh
    Revert to 7.1.6.1

--- obj/build.sh
+++ obj/build.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 # this file is for any additional pipeline commands
 ls /app
+<<<<<<< HEAD
 curl -o /tmp/unifi.zip https://dl.ui.com/unifi/7.1.66-c70daa41cf/UniFi.unix.zip
+=======
+curl -o /tmp/unifi.zip https://dl.ui.com/unifi/7.0.25-43e7fc6711/UniFi.unix.zip
+>>>>>>> parent of 54c68a7 (Update to 7.1.6.1)
 unzip  /tmp/unifi.zip -d /opt
 
 sleep 5
