1ba434fdc32e1f303c7d18d7940949757a24e62c
Scobber <scott@pep.id.au>
2022-10-25 11:10:16 +1100

    Push to 7.2.94
    Push 7.2.94

--- obj/build.sh
+++ obj/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # this file is for any additional pipeline commands
 ls /app
-curl -o /tmp/unifi.zip https://dl.ui.com/unifi/7.1.66-c70daa41cf/UniFi.unix.zip
+curl -o /tmp/unifi.zip https://dl.ui.com/unifi/7.2.94/UniFi.unix.zip
 unzip  /tmp/unifi.zip -d /opt
 
 sleep 5
