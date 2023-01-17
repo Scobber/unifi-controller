7840d059cc901f718ae2b563bd102288af2f5c4c
Scobber <4395760+Scobber@users.noreply.github.com>
2023-01-17 13:02:08 +1100

    Update build.sh

--- obj/build.sh
+++ obj/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # this file is for any additional pipeline commands
 ls /app
-curl -o /tmp/unifi.zip https://dl.ui.com/unifi/7.3.81-1529bd4a64/UniFi.unix.zip
+curl -o /tmp/unifi.zip curl -o /tmp/unifi.zip https://dl.ui.com/unifi/7.2.94/UniFi.unix.zip
 unzip  /tmp/unifi.zip -d /opt
 
 sleep 5
